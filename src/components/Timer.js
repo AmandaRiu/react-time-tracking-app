2e7bc10b3e5ddc2910c03b1629897e3601f58c4a
Amanda Riu <amanda.riu@automattic.com>
2021-01-27 15:20:24 -0500

    Convert Timer to React class to use lifecycle methods in next section of book

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -2,45 +2,53 @@ import React from 'react';
 
 import { renderElapsedString } from "../utils/helpers";
 
-export default function Timer( props ) {
-    const elapsedString = renderElapsedString( props.elapsed );
+export default class Timer extends React.Component {
+    constructor( props ) {
+        super( props );
 
-    const handleTrashClick = () => {
-        props.onTrashClick( props.id );
+        this.elapsedString = renderElapsedString( this.props.elapsed );
+        console.log( this.elapsedString );
+    }
+
+
+    handleTrashClick = () => {
+        this.props.onTrashClick( this.props.id );
     };
 
-    return (
-        <div className='ui centered card'>
-            <div className='content'>
-                <div className='header'>
-                    { props.title }
+    render() {
+        return (
+            <div className='ui centered card'>
+                <div className='content'>
+                    <div className='header'>
+                        { this.props.title }
+                    </div>
+                    <div className='meta'>
+                        { this.props.project }
+                    </div>
+                    <div className='center aligned description'>
+                        <h2>
+                            { this.elapsedString }
+                        </h2>
+                    </div>
+                    <div className='extra content'>
+                        <span
+                            className='right floated edit icon'
+                            onClick={ this.props.onEditClick }
+                        >
+                            <i className='edit icon' />
+                        </span>
+                        <span
+                            className='right floated trash icon'
+                            onClick={ this.handleTrashClick }
+                        >
+                            <i className='trash icon' />
+                        </span>
+                    </div>
                 </div>
-                <div className='meta'>
-                    { props.project }
-                </div>
-                <div className='center aligned description'>
-                    <h2>
-                        { elapsedString }
-                    </h2>
-                </div>
-                <div className='extra content'>
-                    <span
-                        className='right floated edit icon'
-                        onClick={ props.onEditClick }
-                    >
-                        <i className='edit icon' />
-                    </span>
-                    <span
-                        className='right floated trash icon'
-                        onClick={ handleTrashClick }
-                    >
-                        <i className='trash icon' />
-                    </span>
-                </div>
+                <div className='ui bottom attached blue basic button'>
+                    Start
             </div>
-            <div className='ui bottom attached blue basic button'>
-                Start
             </div>
-        </div>
-    );
+        );
+    }
 }
