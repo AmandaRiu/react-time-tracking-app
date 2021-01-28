bfd17adf5de3808132dafed18ceafed1ce6b3b07
Amanda Riu <amanda.riu@automattic.com>
2021-01-27 20:37:04 -0500

    Refactored app.js as a react component class

--- src/App.js
+++ src/App.js
@@ -1,114 +1,110 @@
 import './App.css';
-import React, { useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
+import React from 'react';
 
 import EditableTimerList from './components/EditableTimerList';
 import ToggleableTimerForm from './components/ToggleableTimerForm';
 import { newTimer } from './utils/helpers';
 
-function App() {
-  const [ timers, setTimers ] = useState( [
-    {
-      title: 'Practice squat',
-      project: 'Gym chores',
-      id: uuidv4(),
-      elapsed: 5456099,
-      runningSince: Date.now(),
-    },
-    {
-      title: 'Bake squash',
-      project: 'Kitchen chores',
-      id: uuidv4(),
-      elapsed: 1273998,
-      runningSince: null,
-    },
-  ] );
-
-  const handleCreateFormSubmit = ( timer ) => {
-    createTimer( timer );
+export default class App extends React.Component {
+  state = {
+    timers: [],
   };
 
-  const createTimer = ( timer ) => {
+  handleCreateFormSubmit = ( timer ) => {
+    this.createTimer( timer );
+  };
+
+  createTimer = ( timer ) => {
     const t = newTimer( timer );
-    setTimers( timers.concat( t ) );
+    this.setState( {
+      timers: this.state.timers.concat( t )
+    } );
   };
 
-  const handleEditFormSubmit = ( attrs ) => {
-    updateTimer( attrs );
+  handleEditFormSubmit = ( attrs ) => {
+    this.updateTimer( attrs );
   };
 
-  const handleTrashClick = ( timerId ) => {
-    setTimers( timers.filter( t => t.id !== timerId ) );
+  handleTrashClick = ( timerId ) => {
+    this.setState( {
+      timers: this.state.timers.filter( t => t.id !== timerId )
+    } );
   };
 
-  const handleStartClick = ( timerId ) => {
-    startTimer( timerId );
+  handleStartClick = ( timerId ) => {
+    this.startTimer( timerId );
   };
 
-  const handleStopClick = ( timerId ) => {
-    stopTimer( timerId );
+  handleStopClick = ( timerId ) => {
+    this.stopTimer( timerId );
   };
 
-  const startTimer = ( timerId ) => {
+  startTimer = ( timerId ) => {
     const now = Date.now();
 
-    setTimers( timers.map( ( timer ) => {
-      if ( timer.id === timerId ) {
-        return Object.assign( {}, timer, {
-          runningSince: now,
-        } );
-      } else {
-        return timer;
-      }
-    } ) );
+    this.setState( {
+      timers: this.state.timers.map( ( timer ) => {
+        if ( timer.id === timerId ) {
+          return Object.assign( {}, timer, {
+            runningSince: now,
+          } );
+        } else {
+          return timer;
+        }
+      } )
+    } );
   };
 
-  const stopTimer = ( timerId ) => {
+  stopTimer = ( timerId ) => {
     const now = Date.now();
 
-    setTimers( timers.map( ( timer ) => {
-      if ( timer.id === timerId ) {
-        const lastElapsed = now - timer.runningSince;
-        return Object.assign( {}, timer, {
-          elapsed: timer.elapsed + lastElapsed,
-          runningSince: null,
-        } );
-      } else {
-        return timer;
-      }
-    } ) );
+    this.setState( {
+      timer: this.state.timers.map( ( timer ) => {
+        if ( timer.id === timerId ) {
+          const lastElapsed = now - timer.runningSince;
+          return Object.assign( {}, timer, {
+            elapsed: timer.elapsed + lastElapsed,
+            runningSince: null,
+          } );
+        } else {
+          return timer;
+        }
+      } )
+    } );
   };
 
-  const updateTimer = ( attrs ) => {
-    setTimers( timers.map( ( timer ) => {
-      if ( timer.id === attrs.id ) {
-        return Object.assign( {}, timer, {
-          title: attrs.title,
-          project: attrs.project,
-        } );
-      } else {
-        return timer;
-      }
-    } ) );
+  updateTimer = ( attrs ) => {
+    this.setState( {
+      timers: this.state.timers.map( ( timer ) => {
+        if ( timer.id === attrs.id ) {
+          return Object.assign( {}, timer, {
+            title: attrs.title,
+            project: attrs.project,
+          } );
+        } else {
+          return timer;
+        }
+      } )
+    } );
   };
 
-  return (
-    <div className="ui three column centered grid">
-      <div className="column">
-        <EditableTimerList
-          timers={ timers }
-          onFormSubmit={ handleEditFormSubmit }
-          onTrashClick={ handleTrashClick }
-          onStartClick={ handleStartClick }
-          onStopClick={ handleStopClick }
-        />
-        <ToggleableTimerForm
-          onFormSubmit={ handleCreateFormSubmit }
-          isOpen={ false }
-        />
+  render() {
+    return (
+      <div className="ui three column centered grid">
+        <div className="column">
+          <EditableTimerList
+            timers={ this.state.timers }
+            onFormSubmit={ this.handleEditFormSubmit }
+            onTrashClick={ this.handleTrashClick }
+            onStartClick={ this.handleStartClick }
+            onStopClick={ this.handleStopClick }
+          />
+          <ToggleableTimerForm
+            onFormSubmit={ this.handleCreateFormSubmit }
+            isOpen={ false }
+          />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
-
-export default App;
